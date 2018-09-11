1e5678da3d8bcb9abae43a3afa2d3715103bc8db
jdelagal <jdelagal@gmail.com>
2018-09-11 14:15:17 +0200

    Initial import.

---
+++ src/app/components/tienda/tienda.component.ts
@@ -0,0 +1,16 @@
+import {Component} from '@angular/core';
+
+@Component({
+    selector: 'tienda',
+    templateUrl: './tienda.component.html',
+    styleUrls: ['./tienda.component.css']
+    
+})
+
+export class TiendaComponent {
+    public titulo;
+
+    constructor(){
+        this.titulo = 'Esta es la tienda';
+    }
+}
