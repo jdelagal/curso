a707baeda741ab16ee17db5b886090ea92fb5435
jdelagal <jdelagal@gmail.com>
2018-09-18 12:46:23 +0200

    maquetacion home.

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 
 export class HomeComponent implements OnInit {
-  title = 'Home';
+  title = 'Bienvenido a NGZOO';
   
   ngOnInit(){
     console.log("HomeComponent cargado");
